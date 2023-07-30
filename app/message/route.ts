e30e56f44e220cf0f38711d892cd47c33af14733
Arafatulla <sharullmusicplaylist@gmail.com>
2023-07-31 01:40:26 +0530

    Update route.ts

--- app/message/route.ts
+++ app/message/route.ts
@@ -10,7 +10,7 @@ export async function POST(request: Request) {
   return NextResponse.json({ res });
 }
 
-export async function GET() {
+export async function GET(request: Request) {
   const res = await prisma.message.findMany();
   return NextResponse.json({ res });
 }
